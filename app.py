8683ba4022d59908857fa41a86fdea4f46daf83c
Студент Факультета Бизнеса <student@FB.LOCAL>
2025-09-06 10:33:26 +0700

    Первое приложение 2

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def start():
-    return "web сервер на flask"
+def start():    
     
+    return "<!doctype html>"\
+        "<html>"\
+        "   <body>"\
+        "       <h1>web-сервер на flask</h1>"\
+        "   </body>"\
+        "</html>"
