67217b453d1935be2b1784f1cd76edb2e422f6f9
Shevchenko Ivan <shevchenkoivan@MacBook-Air-Shevchenko.local>
2025-09-20 09:51:08 +0700

    Имя студента

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, redirect, abort
+from flask import Flask, url_for, request, redirect, abort, render_template
 import datetime
 app = Flask(__name__)
 
@@ -349,3 +349,8 @@ def add_flower(name):
     <p>Полный список: {flower_list}</p>
     </body>
 </html>'''
+
+@app.route('/lab2/example')
+def example():
+    name = 'Шевченко Иван'
+    return render_template('example.html', name = name)
