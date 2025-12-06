74ca7908c39ca3cdde2477d09dc1aeb42840efb8
ayvayvmvay <ayvayvmvay@gmail.com>
2025-12-06 21:52:48 +0700

    правки

--- app.py
+++ app.py
@@ -22,6 +22,23 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретный_ключ123')
 app.config['DB_TYPE'] = 'sqlite'
+
+if app.config['DB_TYPE'] == 'postgres':
+  db_name = 'daniil_volkov_orm'
+  db_user = 'daniil_volkov_orm'
+  db_password = '123'
+  host_ip = '127.0.0.1'
+  host_port = 5432
+  app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+  dir_path = path.dirname(path.realpath(__file__))
+  db_path = path.join(dir_path, "daniil_volkov_orm.db")
+  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+
+db.init_app(app)
+
+
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
